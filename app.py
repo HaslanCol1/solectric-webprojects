d20688c36eaf7b1cf3804f87ea351be7d8cfc0bf
Harlem Hernandez <harlemj-hernandezm@unilibre.edu.co>
2025-09-20 15:39:05 -0500

    Frontend por terminar

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def main():
 def auth():
     return render_template("Vistas/auth.html")
 
+@app.route("/portalciudadano")
+def portalciudadano():
+    return render_template("main/port-ciudadano.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
