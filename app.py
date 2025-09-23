8acb44d028d9eb05ba43834da4c30ce809497c4b
Harlem Hernandez <harlemj-hernandezm@unilibre.edu.co>
2025-09-23 06:59:02 -0500

    Organizacion Estructura Componenetes HTML

--- app.py
+++ app.py
@@ -5,15 +5,7 @@ from Config.db import app
 
 @app.route("/main")
 def main():
-    return render_template("main/index.html")
-
-@app.route("/auth")
-def auth():
-    return render_template("Vistas/auth.html")
-
-@app.route("/portalciudadano")
-def portalciudadano():
-    return render_template("main/port-ciudadano.html")
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
